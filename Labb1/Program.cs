817a1c21a84f0e83f2350e4ec172fe3b0e0ad42b
Tim Toresson <timtoresson@hotmail.com>
2021-01-21 16:29:39 +0100

    Edited Labb 1 (removed some comments)

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -5,22 +5,13 @@ namespace Labb1
 {
     class Program
     {
-        //        Skapa en konsollapplikation som tar en textsträng(string) som argument.Den kan
-        //också läsa in det via Console.ReadLine(), men det räcker med en hårdkodad
-        //textsträng.
-        //Textsträngen ska sedan sökas igenom efter alla delsträngar som är tal som börjar
-        //och slutar på samma siffra, utan att start/slutsiffran, eller något annat tecken än
-        //siffror förekommer där emellan.
-        //ex. 3463 är ett korrekt sådant tal, men 34363 är det inte eftersom det finns
-        //ytterligare en 3:a i talet, förutom start och slutsiffran. Strängar med bokstäver i
-        //t.ex 95a9 är inte heller ett korrekta tal.
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter a string");
             string input = Console.ReadLine();
             char[] charArray = input.ToCharArray(); // Dela upp strängen i en char array för enkel genomsökning
             int notImportant; // Någon int-variabel att sätta i out parametern för int.TryParse 
-            List<string> stringList = new List<string>();
+            List<string> stringList = new List<string>(); // Lista att stoppa alla lyckade delsträngar i
             for (int i = 0; i < charArray.Length; i++)
             {
                 // Om charArray[i] är en int så börjar vi söka igenom hela arrayen efter en likadan int
